82ae4f9558c14a5647f21c5bd30799cf686e9724
Izadora Braga <izadora_abraga@outlook.com>
2024-08-19 18:22:01 -0300

    requisito 2

--- news/models.py
+++ news/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
