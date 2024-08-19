b12b3c7c8a1f7c990410d39a960d9ab6af84e362
Izadora Braga <izadora_abraga@outlook.com>
2024-08-19 18:51:39 -0300

    requisito 3

--- news/models.py
+++ news/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
 class Category(models.Model):
@@ -16,3 +18,20 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def validate_title(value):
+    if len(value.split()) < 2:
+        raise ValidationError(_("O título deve conter pelo menos 2 palavras."))
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200, validators=[validate_title])
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    image = models.ImageField(upload_to='img/', blank=True)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
