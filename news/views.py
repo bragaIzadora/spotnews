14df6097c6c3c7096125be0476ce65891c7a541e
Izadora Braga <izadora_abraga@outlook.com>
2024-08-20 17:38:11 -0300

    requisito 5

--- news/views.py
+++ news/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from news.models import News
 
 
 def index(request):
     news_list = News.objects.all()
     return render(request, 'home.html', {"news_list": news_list})
+
+
+def news_details(request, id):
+    news = get_object_or_404(News, pk=id)
+    return render(request, 'news_details.html', {'news': news})
