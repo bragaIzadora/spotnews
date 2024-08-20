60a61581d1a1a5ae71e8aad4289965a914e89265
Izadora Braga <izadora_abraga@outlook.com>
2024-08-20 18:04:19 -0300

    requisito 6

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from news.models import News
+from .forms import CategoryForm  # Importe o formulário
 
 
 def index(request):
@@ -10,3 +11,15 @@ def index(request):
 def news_details(request, id):
     news = get_object_or_404(News, pk=id)
     return render(request, 'news_details.html', {'news': news})
+
+
+def category_form(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home-page')
+    else:
+        form = CategoryForm()
+
+    return render(request, 'categories_form.html', {'form': form})
